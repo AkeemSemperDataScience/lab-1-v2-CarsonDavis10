4c94b5519e79974abba65eb5de0bbd4ec2b24dd5
CarsonDavis10 <carsontrdavis@gmail.com>
2024-09-10 22:28:27 -0600

    test

--- START_Lab_1.py
+++ START_Lab_1.py
@@ -4,14 +4,14 @@ def lab1Question1(input_gb):
     # Do the work here
     # The solution to this goes here (and in all of them below...)
     # Set the variable num_bytes to the answer and return it
-
+    num_bytes = input_gb * 1024 * 1024 * 1024
     return num_bytes
 
 def lab1Question2(name):
     # Take an input of a name, return True if there is an odd number of characters in the name, False otherwise
     # Return None if the input is not a string
     is_odd = None
-
+    
     return is_odd
 
 def lab1Question3(input_string, input_number):
