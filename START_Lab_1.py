c17d565fe2c94f76a5de5cf3d3020609ec5a132f
CarsonDavis10 <carsontrdavis@gmail.com>
2024-09-10 23:13:27 -0600

    test

--- START_Lab_1.py
+++ START_Lab_1.py
@@ -40,9 +40,9 @@ def lab1Question6(quarters, dimes, nickels, pennies):
     # Return the total amount in dollars
     # For example, if the handful contains 4 quarters, 3 dimes, 2 nickels, and 1 penny, the function should return 1.41.
     total = None
-    quarters = 1
-    dimes = 1
-    nickels = 1
+    quarters = 4
+    dimes = 3
+    nickels = 2
     pennies = 1
     total = (quarters * 0.25)+(dimes * 0.10)+(nickels * 0.05)+(pennies * 0.01)
     return total
