8b902f55aa719dbdcdeab6175c3b70e85949311d
CarsonDavis10 <carsontrdavis@gmail.com>
2024-09-15 15:46:37 -0600

    test

--- START_Lab_1.py
+++ START_Lab_1.py
@@ -16,7 +16,7 @@ def lab1Question2(name):
     if len(name) % 2 == 0:
         return False
     else:
-        return None
+        return is_odd
  
 def lab1Question3(input_string, input_number):
     # Take in two inputs - a string and a number
